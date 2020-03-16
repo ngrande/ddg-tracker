3fd278cb2a08c5e998fd2f73f7f2dbf538fcf2c4
ngrande <devlup@outlook.com>
2020-03-16 19:34:55 +0100

    Adjust filter and exclude CDNs

--- generate.py
+++ generate.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 
 import datetime
+import sys
 import os
 import json
 import argparse
 
 
+def dump(json_data):
+	for key, val in json_data.items():
+		print("[{}]: {}".format(key, val))
+
+def is_whitelisted(categories, whitelist):
+	for white in whitelist:
+		if white in categories:
+			return True
+	return False
+
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("path", help="path to the domain files", type=str)
+	parser.add_argument("--exclude", help="comma separated list of domains to exclude (from blocking)", type=str)
 	parser.add_argument("--categories", help="comma separated list of categories to block", type=str)
+	parser.add_argument("--categories-w", help="comma separated list of categories to whitelist", type=str)
 	parser.add_argument("--output", help="absolute output file path", type=str, default="./duckduckgo_tracker.list")
 	args = parser.parse_args()
 
@@ -23,8 +37,12 @@ if __name__ == '__main__':
 							"obscure ownership",
 							"third-party analytics marketing",
 							"unknown high risk behavior"]
+	categories_whitelist = ["CDN"]
+
 	if args.categories:
 		categories_to_block = args.categories.split(",")
+	if args.categories_w:
+		categories_whitelist = args.categories_w.split(",")
 
 	path = args.path
 	domains = set()
@@ -36,14 +54,24 @@ if __name__ == '__main__':
 			domain_json = json.load(f)
 
 			categories = domain_json['categories']
-			print(categories)
+			domain = domain_json['domain']
+
+			if is_whitelisted(categories, categories_whitelist):
+				print("white listed domain: {}".format(domain))
+				continue
+
 			for cat in categories:
 				cat = cat.lower()
 				if cat in categories_to_block:
-					domain = domain_json['domain']
 					domains.add(domain)
 					print("adding domain: {}".format(domain))
 					break
+
+	if args.exclude:
+		for exclude in args.exclude.split(","):
+			if exclude not in domains:
+				continue
+			domains.remove(exclude)
 
 	with open(args.output, "w", encoding="utf-8") as dest:
 		dest.write("# generated at {}\n".format(datetime.datetime.now()))
